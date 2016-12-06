fc5ad897c97c262691d991868fdc8c00654f7485
Mirko Cukich <mirkocukich@gmail.com>
2016-12-06 15:04:27 -0600

    Default Constructor Complete

--- src/com/digitalmirko/Main.java
+++ src/com/digitalmirko/Main.java
@@ -2,10 +2,16 @@ package com.digitalmirko;
 
 // Java Constructor Example
 
-
 public class Main {
 
+    public Main(){
+        System.out.println("Inside the default constructor");
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        Main main = new Main();
     }
 }
+
+// output:  Inside the default constructor
